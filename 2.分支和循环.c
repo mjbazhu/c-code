38420baf7a3742c2fd31cdf3105400f794e794ea
mjbazhu <100551328+mjbazhu@users.noreply.github.com>
2022-02-28 14:06:33 +0800

    Update 2.分支和循环.c

--- 2.分支和循环.c
+++ 2.分支和循环.c
@@ -1,1 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+
+int main()
+{
+	int m = 1;
+	int n = 2;
+	if (1==m)//常数写到左边
+	{
+		if (1 == n)
+			printf("%d\n", n);
+	}
+	else
+		printf("失败");
+	return 0;
+}
