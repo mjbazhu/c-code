1fa346274144598dd17522dd5c079709fa6e6cbb
mjbazhu <mj1724570407>
2022-03-04 00:53:54 +0800

    x

---
+++ 10.基本使用.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//①strcpy
+//strcpy(str1,str2)
+会将字符串str2复制给字符串str1
+因为字符串以\0结尾，所以\0也会复制过去
+字符串结束标志\0，所以拷贝后\0后面的内容不会显示
+int main()
+{
+	char str1[] = "hello";
+	char str2[20];
+	char str3[40];
+	strcpy(str2,str1);
+	strcpy(str3,"hi,baby");
+	printf("%s\n%s\n%s\n", str1, str2, str3);
+	return 0;
+}
+
+//②memset(str,value,num)
+//用num个value替换str的num个值
